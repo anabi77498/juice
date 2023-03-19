667e109fcf847bef131bdce2465c183a1a9956aa
nma22 <95200589+nma22@users.noreply.github.com>
2023-03-18 21:26:59 -0400

    Nm542/account (#6)
    * Outlined interface of account
    
    * Add JSON data folder
    
    * Add Finance library opam file
    
    * Edited dune files
    
    * Implement Account.from_json, Account.owner
    
    - Replaced release code implementation with my own
    - Added a test case
    
    * Implement Account.create_account
    
    - Replaced release code implementation with my own
    - Added a test case
    
    * Implement Account.account_type,status
    
    * Implement Account.assets_value
    
    * Implement Account.withdraw
    
    * Implement Account.deposit
    
    * Implement Account.close
    
    * Fix account.mli bug
    
    * Remove code.ml
    
    * Implement Account.transaction_of_json
    
    * Added abstract stock type
    
    * Added name to README (#7)
    
    * Implement Account.stock_of_json
    
    * Fixed bugs in Account.stock/transaction_of_json
    
    * Added all_accounts class variable

--- src/account.ml
+++ src/account.ml
@@ -1,3 +1,7 @@
+exception LimitExceeded of int
+exception MaximumExceeded of int
+exception InactiveAccount
+
 type account =
   | Savings
   | Checking
@@ -6,11 +10,11 @@ type account =
 type status =
   | Active
   | Inactive
-  | Closed
 
-type asset = {
-  category : string;
-  current_value : int;
+type stock = {
+  shares : int;
+  cur_value_share : int;
+  api_access : string;
 }
 
 type transaction = {
@@ -19,18 +23,34 @@ type transaction = {
 }
 
 type t = {
+  id : int;
   owner : string;
   account_type : account;
   status : status;
   balance : int;
   limit : int;
   maximum : int;
-  assets : asset list;
+  stocks : stock list;
   history : transaction list;
 }
 
-let asset_of_json = []
-let transaction_of_json = []
+let all_accounts : t list ref = ref []
+
+let rec stock_of_json j =
+  let open Yojson.Basic.Util in
+  {
+    shares = j |> member "Shares" |> to_int;
+    cur_value_share = j |> member "Current Value Share" |> to_int;
+    api_access = j |> member "API Access" |> to_string;
+  }
+
+let transaction_of_json j =
+  let open Yojson.Basic.Util in
+  let amt = j |> member "amount" |> to_int in
+  {
+    transaction_type = (if amt > 0 then "Withdrawal" else "Deposit");
+    amount = amt;
+  }
 
 let parse_acc_type = function
   | "Savings" -> Savings
@@ -41,68 +61,150 @@ let parse_acc_type = function
 let parse_stat_type = function
   | "Active" -> Active
   | "Inactive" -> Inactive
-  | "Closed" -> Closed
   | _ -> failwith "This should never happen, given from_json's precondition"
 
 let from_json json =
   let open Yojson.Basic.Util in
-  {
-    owner = json |> to_assoc |> List.assoc "owner" |> to_string;
-    account_type =
-      json |> to_assoc |> List.assoc "account_type" |> to_string
-      |> parse_acc_type;
-    status =
-      json |> to_assoc |> List.assoc "status" |> to_string |> parse_stat_type;
-    balance = json |> to_assoc |> List.assoc "balance" |> to_int;
-    limit = json |> to_assoc |> List.assoc "limit" |> to_int;
-    maximum = json |> to_assoc |> List.assoc "maximum" |> to_int;
-    assets =
-      [] (*json |> member "assets" |> to_list |> List.map asset_of_json;*);
-    history =
-      []
-      (*json |> member "history" |> to_list |> List.map transaction_of_json;*);
-  }
+  let new_acc =
+    {
+      id = List.length !all_accounts;
+      owner = json |> to_assoc |> List.assoc "owner" |> to_string;
+      account_type =
+        json |> to_assoc |> List.assoc "account_type" |> to_string
+        |> parse_acc_type;
+      status =
+        json |> to_assoc |> List.assoc "status" |> to_string |> parse_stat_type;
+      balance = json |> to_assoc |> List.assoc "balance" |> to_int;
+      limit = json |> to_assoc |> List.assoc "limit" |> to_int;
+      maximum = json |> to_assoc |> List.assoc "maximum" |> to_int;
+      stocks = json |> member "stocks" |> to_list |> List.map stock_of_json;
+      history =
+        json |> member "history" |> to_list |> List.map transaction_of_json;
+    }
+  in
+  all_accounts := new_acc :: !all_accounts;
+  new_acc.id
 
 let create_account owner acc_type balance limit maximum =
-  {
-    owner;
-    account_type = acc_type |> parse_acc_type;
-    status = Active;
-    balance;
-    limit;
-    maximum;
-    assets = [];
-    history = [];
-  }
-
-let owner acc = acc.owner
-let account_type acc = acc.account_type
-let status acc = acc.status
-let balance acc = acc.balance
-let limit acc = acc.limit
-let maximum acc = acc.maximum
-
-let rec assets_value = function
-  | [] -> 0
-  | h :: t -> h.current_value + assets_value t
-
-let withdraw acc n =
-  if n <= acc.limit then
+  assert (limit >= 0);
+  let new_acc =
     {
-      acc with
-      balance = acc.balance - n;
-      limit = acc.limit - n;
-      history = { transaction_type = "Withdrawal"; amount = n } :: acc.history;
+      id = List.length !all_accounts;
+      owner;
+      account_type = acc_type |> parse_acc_type;
+      status = Active;
+      balance;
+      limit;
+      maximum;
+      stocks = [];
+      history = [];
     }
-  else acc
+  in
+  all_accounts := new_acc :: !all_accounts;
+  new_acc.id
 
-let deposit acc n =
-  {
-    acc with
-    balance = acc.balance + n;
-    history = { transaction_type = "Deposit"; amount = n } :: acc.history;
-  }
+let get_acc id = List.nth !all_accounts id
 
-let close acc = { acc with status = Closed }
+let update_all_accounts id new_acc =
+  all_accounts :=
+    List.mapi (fun i x -> if i = id then new_acc else x) !all_accounts
 
-(*let latest_transaction acc = List.head acc.history*)
+let owner id =
+  let acc = get_acc id in
+  acc.owner
+
+let account_type id =
+  let acc = get_acc id in
+  acc.account_type
+
+let status id =
+  let acc = get_acc id in
+  acc.status
+
+let balance id =
+  let acc = get_acc id in
+  acc.balance
+
+let limit id =
+  let acc = get_acc id in
+  acc.limit
+
+let maximum id =
+  let acc = get_acc id in
+  acc.maximum
+
+let stocks id =
+  (*when writing interface, do not include this function, its not available to
+    the user*)
+  let acc = get_acc id in
+  acc.stocks
+
+let stocks_value id =
+  let rec helper lst =
+    match lst with
+    | [] -> 0
+    | h :: t -> (h.cur_value_share * h.shares) + helper t
+  in
+  id |> stocks |> helper
+
+let withdraw id n =
+  let acc = get_acc id in
+  let new_acc =
+    if acc.status <> Active then raise InactiveAccount
+    else if acc.balance - n < acc.limit then raise (LimitExceeded n)
+    else if n <= acc.maximum then
+      {
+        acc with
+        balance = acc.balance - n;
+        history = { transaction_type = "Withdrawal"; amount = n } :: acc.history;
+      }
+    else raise (MaximumExceeded n)
+  in
+  update_all_accounts id new_acc
+
+let deposit id n =
+  let acc = get_acc id in
+  let new_acc =
+    if acc.status <> Active then raise InactiveAccount
+    else
+      {
+        acc with
+        balance = acc.balance + n;
+        history = { transaction_type = "Deposit"; amount = n } :: acc.history;
+      }
+  in
+  update_all_accounts id new_acc
+
+let activate id =
+  let acc = get_acc id in
+  let new_acc = { acc with status = Active } in
+  update_all_accounts id new_acc
+
+let deactivate id =
+  let acc = get_acc id in
+  let new_acc = { acc with status = Inactive } in
+  update_all_accounts id new_acc
+
+let transfer id1 id2 n =
+  let acc1 = get_acc id1 in
+  let acc2 = get_acc id2 in
+  let new_acc1 =
+    {
+      acc1 with
+      balance = acc1.balance - n;
+      history =
+        { transaction_type = "Transfer Send"; amount = n } :: acc1.history;
+    }
+  in
+  let new_acc2 =
+    {
+      acc2 with
+      balance = acc2.balance + n;
+      history =
+        { transaction_type = "Transfer Recieve"; amount = n } :: acc2.history;
+    }
+  in
+  update_all_accounts id1 new_acc1;
+  update_all_accounts id2 new_acc2
+
+(*let latest_transaction id = let acc = get_acc id in List.head acc.history*)
